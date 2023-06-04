9c34178401a0b51ef2ef08368c19fab770b59f3d
Elinaldo Monteiro <elinaldosoft@gmail.com>
2023-06-04 14:31:58 -0300

    Add gunicorn and gevent, config to nginx and supervisor. Enable config to production

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,2 +1,29 @@
 from config.settings.base import * # noqa
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['gastemenos.club']
+
+BASE_LOG = '/home/ubuntu/logs'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': f"{BASE_LOG}/debug.log"
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+        'django.template': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+        },
+    },
+}
