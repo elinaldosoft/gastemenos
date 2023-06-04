d9c6cdc66b8f1b171406d369cb5d2a365ee3c5f2
Elinaldo Monteiro <elinaldosoft@gmail.com>
2023-06-04 14:43:08 -0300

    Add alow csrf strusted to gastemenos.club and subdomain

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,7 @@ from config.settings.base import * # noqa
 DEBUG = False
 
 ALLOWED_HOSTS = ['gastemenos.club']
+CSRF_TRUSTED_ORIGINS = ['https://*.gastemenos.club']
 
 BASE_LOG = '/home/ubuntu/logs'
 
