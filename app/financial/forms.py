23ede287476da01989f35fb1e5b626f3847fa679
Elinaldo Monteiro <elinaldosoft@gmail.com>
2023-06-04 14:46:23 -0300

    Add required false to new expense in field paid_at

--- app/financial/forms.py
+++ app/financial/forms.py
@@ -7,7 +7,7 @@ class ExpenseForm(forms.ModelForm):
     title = forms.CharField(label="Título", max_length=100, widget=forms.TextInput(attrs={"class": "form-control"}))
     amount = forms.DecimalField(label="Valor", max_digits=10, decimal_places=2, widget=forms.NumberInput(attrs={"class": "form-control"}))
     expires_at = forms.DateField(label="Vencimento", widget=forms.DateInput(attrs={"class": "form-control", "id": "datepicker"}))
-    paid_at = forms.DateField(label="Está pago?", widget=forms.DateInput(attrs={"class": "form-control", "id": "datepicker_paid_at"}))
+    paid_at = forms.DateField(label="Está pago?", widget=forms.DateInput(attrs={"class": "form-control", "id": "datepicker_paid_at"}), required=False)
     status = forms.ChoiceField(label="Status", choices=STATUS_EXPENSE)
     notes = forms.CharField(label="Observações", widget=forms.Textarea(attrs={"class": "form-control", "rows": 2}), required=False)
 
